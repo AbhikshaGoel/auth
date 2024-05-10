8f97c78e9d83b8cf75722b788436fe452c84d134
Abhiksha Goel <asnemaris@gmail.com>
2024-05-10 16:10:25 +0530

    beautifuy the page

--- app/page.tsx
+++ app/page.tsx
@@ -7,18 +7,18 @@ import { Label } from "@/components/ui/label";
 
 export default function Dashboard() {
   return (
-    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px] bg-[#ece3d6]">
+    <div className="w-full h-screen lg:grid lg:grid-cols-2 bg-[#ece3d6]">
       <div className="flex items-center justify-center py-12">
-        <div className="mx-auto grid w-[350px] gap-6">
+        <div className="mx-auto grid w-[400px] gap-6">
           <div className="grid gap-2 text-center">
-            <h1 className="text-3xl font-bold text-[#c4a97e]">Login</h1>
-            <p className="text-balance text-[#95866f]">
+            <h1 className="text-5xl font-bold text-[#c4a97e]">Login</h1>
+            <p className="text-2xl text-[#95866f]">
               Enter your email below to login to your account
             </p>
           </div>
           <div className="grid gap-4">
             <div className="grid gap-2">
-              <Label htmlFor="email" className="text-[#926e45]">
+              <Label htmlFor="email" className="text-2xl text-[#926e45]">
                 Email
               </Label>
               <Input
@@ -26,17 +26,17 @@ export default function Dashboard() {
                 type="email"
                 placeholder="m@example.com"
                 required
-                className="bg-[#6c5641] text-white"
+                className="text-lg p-3"
               />
             </div>
             <div className="grid gap-2">
               <div className="flex items-center">
-                <Label htmlFor="password" className="text-[#926e45]">
+                <Label htmlFor="password" className="text-2xl text-[#926e45]">
                   Password
                 </Label>
                 <Link
                   href="/forgot-password"
-                  className="ml-auto inline-block text-sm underline text-[#c4a97e]"
+                  className="ml-auto inline-block text-lg underline text-[#c4a97e]"
                 >
                   Forgot your password?
                 </Link>
@@ -45,17 +45,17 @@ export default function Dashboard() {
                 id="password"
                 type="password"
                 required
-                className="bg-[#6c5641] text-white"
+                className="text-lg p-3"
               />
             </div>
             <Button
               type="submit"
-              className="w-full bg-[#c4a97e] text-[#6c5641]"
+              className="h-12 w-full bg-[#c4a97e] text-[#6c5641] text-2xl p-3"
             >
               Login
             </Button>
           </div>
-          <div className="mt-4 text-center text-sm text-[#95866f]">
+          <div className="mt-4 text-center text-lg text-[#95866f]">
             Don&apos;t have an account?{" "}
             <Link href="#" className="underline text-[#c4a97e]">
               Sign up
@@ -63,13 +63,13 @@ export default function Dashboard() {
           </div>
         </div>
       </div>
-      <div className="rounded-lg hidden bg-muted lg:block">
+      <div className="rounded-lg hidden lg:block">
         <Image
           src="/meeting_room.jpeg"
           alt="Image"
           width="1920"
           height="1080"
-          className="rounded-lg yh-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          className="rounded-lg h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
       </div>
     </div>
