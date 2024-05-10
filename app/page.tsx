293cf028c0e55e8ba80a615c764116aadbf708c5
satya <satya14vikas@gmail.com>
2024-05-10 12:50:30 +0530

    changed the text size of login

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Dashboard() {
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
-            <h1 className="text-3xl font-bold text-[#c4a97e]">Login</h1>
+            <h1 className="text-5xl font-bold text-[#c4a97e]">Login</h1>
             <p className="text-balance text-[#95866f]">
               Enter your email below to login to your account
             </p>
