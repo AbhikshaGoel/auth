d165f99730a580a7bda36e438859e5dc8a505b2b
Abhiksha Goel <asnemaris@gmail.com>
2024-05-10 16:45:00 +0530

    adjusting size of login

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,30 @@ import { Label } from "@/components/ui/label";
 
 export default function Dashboard() {
   return (
-    <div className="w-full h-screen lg:grid lg:grid-cols-2 bg-[#ece3d6]">
-      <div className="flex items-center justify-center py-12">
-        <div className="mx-auto grid w-[400px] gap-6">
+    <div className="w-full min-h-screen lg:grid lg:grid-cols-2 bg-[#ece3d6]">
+      <div className="flex items-center justify-center py-8 px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto items-center justify-center grid w-full sm:max-w-md lg:max-w-lg gap-6">
+          <div className="rounded-lg">
+            <Image
+              src="/iocl_logo.png"
+              alt="Image"
+              width="420"
+              height="220"
+              className="rounded-lg object-cover dark:brightness-[0.2]"
+            />
+          </div>
           <div className="grid gap-2 text-center">
-            <h1 className="text-3xl font-bold text-[#c4a97e]">Login</h1>
-            <p className="text-balance text-[#95866f]">
-              Enter your email below to login to your account
-            </p>
+            <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-[#c4a97e]">
+              Login
+            </h1>
           </div>
           <div className="grid gap-4">
             <div className="grid gap-2">
-              <Label htmlFor="email" className="text-2xl text-[#926e45]">
-                Email
+              <Label
+                htmlFor="email"
+                className="text-xl sm:text-2xl text-[#926e45]"
+              >
+                Login Id
               </Label>
               <Input
                 id="email"
@@ -31,7 +42,10 @@ export default function Dashboard() {
             </div>
             <div className="grid gap-2">
               <div className="flex items-center">
-                <Label htmlFor="password" className="text-2xl text-[#926e45]">
+                <Label
+                  htmlFor="password"
+                  className="text-xl sm:text-2xl text-[#926e45]"
+                >
                   Password
                 </Label>
                 <Link
@@ -69,7 +83,7 @@ export default function Dashboard() {
           alt="Image"
           width="1920"
           height="1080"
-          className="rounded-lg h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          className="rounded-lg h-full w-full object-cover dark:brightness-[0.2]"
         />
       </div>
     </div>
