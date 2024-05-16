910d1bc2a2eca2d87534cad758789648dfe0ed91
Abhiksha Goel <asnemaris@gmail.com>
2024-05-16 14:48:10 +0530

    removed old code and directly send data to server

--- actions/login.ts
+++ actions/login.ts
@@ -1,11 +1,4 @@
 "use server";
-import { signIn } from "@/auth";
-
-import { getUserByEmail } from "@/data/user";
-import prisma from "@/lib/db";
-
-import bcrypt from "bcryptjs";
-import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { LoginSchema } from "@/schemas";
 import { AuthError } from "next-auth";
 
@@ -21,43 +14,5 @@ export const login = async (
     };
   }
   const { email, password, code } = validatedFields.data;
-  const existingUser = await getUserByEmail(email);
-  if (!existingUser || !existingUser.password || !existingUser.email) {
-    return {
-      error: "Invalid credentials",
-    };
-  }
-
-  // check if password exists compare before sending 2FA token
-  if (existingUser.password) {
-    const passwordMatch = await bcrypt.compare(password, existingUser.password);
-    if (!passwordMatch) {
-      return {
-        error: "Invalid credentials",
-      };
-    }
-  }
-
-  try {
-    await signIn("credentials", {
-      email,
-      password,
-      redirectTo: callBackUrl || DEFAULT_LOGIN_REDIRECT,
-    });
-  } catch (error) {
-    if (error instanceof AuthError) {
-      switch (error.type) {
-        case "CredentialsSignin":
-          return {
-            error: "Invalid credentials",
-          };
-
-        default:
-          return {
-            error: "Something went wrong",
-          };
-      }
-    }
-    throw error;
-  }
+  //const existingUser = await getUserByEmail(email);
 };
